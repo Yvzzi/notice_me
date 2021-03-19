c39206bdf3c790158a82c93b206a49262f3a21d4
Yvzzi <2433988494@qq.com>
2021-03-19 11:50:11 +0800

    feature: report job name

--- notice_me.py
+++ notice_me.py
@@ -17,34 +17,46 @@ fp = os.popen(
     "export LSF_BINDIR=" + LSF_BINDIR + ";" +
     "export LSF_LIBDIR=" + LSF_LIBDIR + ";" +
     "export LSF_SERVERDIR=" + LSF_SERVERDIR + ";" +
-    BJOB_PATH + " -u " + USER
+    BJOB_PATH + " -u " + USER + " 2>&1"
 )
 ret_str = fp.read()
 fp.close()
 
+job_id_dict = {}
 job_ids = []
 if not ret_str.startswith("No"):
-    lines = ret_str.splitlines()[1:]
+    lines = ret_str.splitlines()
+    start_index = lines[0].index("JOB_NAME")
+    lines = lines[1:]
     for line in lines:
         if line.startswith(" "):
             continue
-        job_ids.append(int(line.split("   ")[0]))
+        job_id = int(line.split("   ")[0])
+        job_name = line[start_index:]
+        end_index = job_name.index(" ")
+        job_name = job_name[:end_index]
+
+        job_id_dict[str(job_id)] = job_name
+        job_ids.append(job_id)
 
 file_path = "data.json"
 if not os.path.exists(file_path):
     fp = open(file_path, "w")
-    fp.write("[]")
+    fp.write("[{},[]]")
     fp.close()
 
 fp = open(file_path, "r+")
-old_job_ids = json.load(fp)
+old_job_id_dict, old_job_ids = json.load(fp)
 fp.truncate(0)
 fp.seek(0)
-json.dump(job_ids, fp)
+json.dump([job_id_dict, job_ids], fp)
 
 finished_job_ids = list(set(old_job_ids) - (set(old_job_ids) & set(job_ids)))
 if len(finished_job_ids) != 0:
+    buf = []
+    for _id in finished_job_ids:
+        buf.append(str(_id) + "(" + (old_job_id_dict[str(_id)].encode("utf-8")) + ")")
     res = requests.post("https://sctapi.ftqq.com/" + SENDKEY + ".send", {
         "text": "超算计算通知",
-        "desp": "任务" + str(finished_job_ids) + "完成了, 快去看看叭"
+        "desp": "任务" + (", ".join(buf)) + "完成了, 快去看看叭"
     }, headers={"content-type": "application/x-www-form-urlencoded"})
